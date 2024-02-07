37a3c3431f5c9812861e52cd29c74ecde66b0858
CanseFr <jucansell@gmail.com>
2024-02-07 14:16:20 +0100

    Ajout de la classe client, manque le lien de Compte

---
+++ Bank/Class/Client.cs
@@ -0,0 +1,10 @@
+namespace Bank.Class;
+
+public class Client
+{
+    private String numClient;
+    private String adress;
+    private String name;
+    private List<Account> accountList;
+    
+}
