edf60829026fe981afc41a284716d4bb8c68a451
CanseFr <jucansell@gmail.com>
2024-02-07 14:19:37 +0100

    Ajout de la reference compte dans la liste

--- Bank/Class/Client.cs
+++ Bank/Class/Client.cs
@@ -5,6 +5,6 @@ public class Client
     private String numClient;
     private String adress;
     private String name;
-    private List<Account> accountList;
+    private List<Compte> accountList;
     
 }
