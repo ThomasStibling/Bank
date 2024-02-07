78c79536f46af72194719ce25c9f6866e9ae11bc
ThomasStibling <thomas.stibling@icloud.com>
2024-02-07 14:20:47 +0100

    Init repo

--- Bank/Class/Compte.cs
+++ Bank/Class/Compte.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 namespace Bank.Class
 {
-	public class Compte
-	{
-		public Compte()
-		{
-		}
-	}
+    public abstract class Compte
+    {
+        protected double Solde { get; set; }
+        protected string NumeroCompte { get; set; }
+
+        public abstract void Debiter(double montant);
+        public abstract void Crediter(double montant);
+    }
 }
 
