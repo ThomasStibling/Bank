ebcd92a918d8dec2d0ec0ed6c9fa7344e541ee7e
Ishak-rav <chennoufishak@gmail.com>
2024-02-08 09:29:54 +0100

    wip

--- Bank/Class/Compte.cs
+++ Bank/Class/Compte.cs
@@ -6,6 +6,16 @@ namespace Bank.Class
         protected double Solde { get; set; }
         protected string NumeroCompte { get; set; }
 
+        public string GetNumeroCompte()
+        {
+            return NumeroCompte;
+        }
+
+        public double GetSolde()
+        {
+            return Solde;
+        }
+
         public abstract void Debiter(double montant);
 
         public  void Crediter(double montant)
